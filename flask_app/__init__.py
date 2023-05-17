62f040425ca9a903c11779861d623c6bbd5714fa
Souheil Khalfallh <khalfallah.souheil@planet.tn>
2023-05-17 16:09:38 +0100

    05/17/2023 adding cors correction

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,6 +1,8 @@
 # __init__.py
 from flask import Flask,render_template, request,redirect
+from flask_cors import CORS
 app = Flask(__name__)
 app.secret_key = "something secret"
+CORS(app)
 
 DATABASE = "tvshows_db"
